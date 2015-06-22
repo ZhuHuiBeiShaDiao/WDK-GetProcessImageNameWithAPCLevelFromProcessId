d93d5daa8f848122fc9c92fd9fcdfda52c3b304e
lain32 <lain32@icloud.com>
2015-06-22 17:44:14 +0900

    내부적으로 사용하고 있는 불필요하게 선언 된 함수를 삭제하였습니다.

--- minifltLib.h
+++ minifltLib.h
@@ -1,43 +1,6 @@
 #ifndef __MINIFLTLIB_H__
 #define __MINIFLTLIB_H__
 
-#define MINIFLT_LIB_TAG 'tlfM'
-
-typedef struct
-{
-	UNICODE_STRING uniName;
-	ANSI_STRING ansiString;
-} NAME_INFORMATION, *PNAME_INFORMATION;
-
-///////////////////////////////////////////////////////////////
-// 파일 이름을 가져오기 위한 함수들이 선언되었습니다.
-///////////////////////////////////////////////////////////////
-PVOID AllocatePoolWithFillZero( 
-	ULONG dwType, 
-	ULONG dwSize 
-	);
-
-NTSTATUS CreateNameInformationMemory( 
-	PNAME_INFORMATION * ppObjNameInfo 
-	);
-
-NTSTATUS DeleteNameInformationMemory( 
-	PNAME_INFORMATION pObjNameInfo 
-	);
-
-NTSTATUS QueryFileName(
-	PFILE_OBJECT pFileObj, 
-	PNAME_INFORMATION pObjNameInfo
-	);
-
-BOOLEAN QueryFileNameEx(__in PFLT_CALLBACK_DATA Data,
-								__in PCFLT_RELATED_OBJECTS FltObjects,
-								__out PFLT_FILE_NAME_INFORMATION *nameInfo,
-								__out PNAME_INFORMATION *pNameInfo,
-								__out PUNICODE_STRING *nameToUse);
-///////////////////////////////////////////////////////////////
-
-
 ///////////////////////////////////////////////////////////////
 // 프로세스 이름을 가져오는 함수입니다.
 ///////////////////////////////////////////////////////////////
